8de318a99aedcad6598c3626f9aa625e53e79c35
gialli24 <a.gialli2409@gmail.com>
2026-02-02 15:27:31 +0100

    Publish exercise

---
+++ script.js
@@ -0,0 +1,26 @@
+// Data Collection 
+// prompt: Chilometri da Percorrere, Età
+let km, age;
+const priceXkilometer = 0.21;
+
+km = parseInt(prompt("Inserisci i chilometri da percorrere:"));
+age = parseInt(prompt("Specifica la tua età:"));
+
+// Calculate final price km * 0.21 €
+let final_price = km * priceXkilometer;
+
+// Logical Area
+// IF age less then 18 -20%
+// IF age at least 65 -40%
+// ELSE final regular price
+
+if (age < 18) {
+    final_price -= final_price * 20 / 100;
+} else if (age >= 65) {
+    final_price -= final_price * 40 / 100;
+}
+
+// Output final price
+let rounded_price = Math.round( final_price * 100 ) / 100;
+console.log(rounded_price);
+
